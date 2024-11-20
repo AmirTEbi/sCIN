6c5b397439270568207e8391e107138675933131
Amir Ebrhaimi <amir.ebrahimi01@ut.ac.ir>
2024-11-20 16:20:19 +0330

    added to modularize code. may be deleted later

---
+++ sc_cool/benchmarks/benchmarks.py
@@ -0,0 +1,34 @@
+"""This is a stand-alone script to perform benchmarking when I don't want to run the pipeline."""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sc_cool.data_processing.plots import plot_recall_at_k, plot_MedR, plot_class_lbl_accuracy
+import argparse
+
+
+def main():
+
+    # Get parameters
+    parser = argparse.ArgumentParser(description='Get Parameters')
+    parser.add_argument('--measure', type=str, help='Measure')
+    parser.add_argument('--data_path', type=str, help='Data Path')
+    parser.add_argument('--save_dir', type=str, help='Save Directory')
+    parser.add_argument('--model_name', type=str, help='Model Name')
+    args = parser.parse_args()
+
+    # Read data
+    data = pd.read_csv(args.data_path)
+    print(data.head())
+
+    # Make plots
+    if args.measure == "recall_at_k":
+        plot_recall_at_k(data, save_dir=args.save_dir, model_name=args.model_name)
+    elif args.measure == "MedR":
+        plot_MedR(data, save_dir=args.save_dir, model_name=args.model_name)    
+    elif args.measure == "Class_lbl_Acc":
+        plot_class_lbl_accuracy(data, save_dir=args.save_dir, model_name=args.model_name)
+
+
+if __name__ == "__main__":
+    main()
