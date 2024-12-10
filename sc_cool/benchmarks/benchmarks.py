ce923ac4ce7876526a73bf522008808b4d7b7324
Amir Ebrhaimi <amir.ebrahimi01@ut.ac.ir>
2024-12-10 11:49:34 +0330

    tutorial and cleanness added

--- sc_cool/benchmarks/benchmarks.py
+++ sc_cool/benchmarks/benchmarks.py
@@ -1,4 +1,20 @@
-"""This is a stand-alone script to perform benchmarking when I don't want to run the pipeline."""
+"""
+This is a stand-alone script to perform benchmarking when I don't want to run the pipeline.
+
+Tutorial:
+
+> cd sc-cool
+
+# Plot a specific metric based on a pre-existing evaluation file 
+sc-cool > python -m sc_cool.benchmarks.benchmarks --save_dir "..." model_name "all" --metric "ASW" --metric_path "..." --plot
+
+# Compute all integration metrics based on pre-existing embeddings
+sc-cool > python -m sc_cool.benchmarks.benchmarks --input_emb_dir "..." --save_dir "..." --model_name "scCOOL" --compute 
+
+# Compute all integration metrics for reverse modality alignment setting. Just add a --reverse flag :) !
+sc-cool > python -m sc_cool.benchmarks.benchmarks --input_emb_dir "..." --save_dir "..." --model_name "scCOOL" --compute --reverse
+
+"""
 
 import numpy as np
 import pandas as pd
@@ -11,22 +27,22 @@ import os
 
 def main():
 
-    # Get parameters
-    parser = argparse.ArgumentParser(description='Get Parameters')
-    parser.add_argument('--input_dir', type=str, help='Data Path')
+    # Get options
+    parser = argparse.ArgumentParser(description='Get Options')
+    parser.add_argument('--input_emb_dir', type=str, help='Input directory for embeddings')
     parser.add_argument('--save_dir', type=str, help='Save Directory')
     parser.add_argument('--model_name', type=str, help='Model Name', default="all")
-    parser.add_argument('--metric', type=str, help='Metric')
-    parser.add_argument('--metric_file', type=str, help='Metric file')
+    parser.add_argument('--metric', type=str, help='Metric to be computed')
+    parser.add_argument('--metric_path', type=str, help='Path to a metric file')
     parser.add_argument('--compute', action='store_true', help='Compute metrics')
-    parser.add_argument('--reverse', action='store_true', help='Compute metrics from RNA to ATAC')
-    parser.add_argument('--plot', action='store_true', help='Plot the specified measure')
+    parser.add_argument('--reverse', action='store_true', help='Compute metrics in reverse setting')
+    parser.add_argument('--plot', action='store_true', help='Plot the specified metric')
     args = parser.parse_args()
 
     # Compute metrics if needed
     if args.compute:
 
-        files = [f for f in os.listdir(args.input_dir) if f.endswith(".npy")]
+        files = [f for f in os.listdir(args.input_emb_dir) if f.endswith(".npy")]
         print(f"Found .npy files: {files}")
         
         files.sort()
@@ -37,9 +53,9 @@ def main():
         results = []
         for rep_idx, (rna_file, atac_file, lbl_file) in enumerate(zip(rna_embs_files, atac_embs_files, lbls_files), start=1):
 
-            rna_path = os.path.join(args.input_dir, rna_file)
-            atac_path = os.path.join(args.input_dir, atac_file)
-            lbls_path = os.path.join(args.input_dir, lbl_file)
+            rna_path = os.path.join(args.input_emb_dir, rna_file)
+            atac_path = os.path.join(args.input_emb_dir, atac_file)
+            lbls_path = os.path.join(args.input_emb_dir, lbl_file)
 
             rna_emb = np.load(rna_path)
             atac_emb = np.load(atac_path)
@@ -59,8 +75,8 @@ def main():
             "Models":args.model_name,
             "Replicates":rep_idx,
             "Recall_at_k":recall_at_k,
-            "Num_pairs":number_of_pairs,
-            "Class_label_acc":ct_acc
+            "num_pairs":number_of_pairs,
+            "cell_type_acc":ct_acc
             })
 
             #break
@@ -75,8 +91,7 @@ def main():
     if args.plot:
 
         # Read data
-        data_path = args.input_dir + "\\" + args.metric_file
-        data = pd.read_csv(data_path)
+        data = pd.read_csv(args.metric_path)
         print(data.head())  
 
         if args.metric == "recall_at_k":
