1737bf8938e5c28b9f57dde442848527ae126ea9
Amir Ebrahimi <amir.ebrahimi01@ut.ac.ir>
2025-03-23 22:44:05 +0330

    plot from csv files

--- plot_metrics.py
+++ plot_metrics.py
@@ -1,14 +1,12 @@
 import pandas as pd 
 import numpy as np 
 from sCIN.plots import (plot_recall_at_k,
-                   plot_asw,
-                   plot_cell_type_accuracy,
-                   plot_median_rank,
-                   compute_plot_tsne_original,
-                   plot_tsne_original,
-                   compute_plot_tsne_embs,
-                   plot_tsne_embs,
-                   plot_all)
+                        plot_asw,
+                        plot_cell_type_accuracy,
+                        plot_median_rank,
+                        plot_tsne_original,
+                        plot_tsne_embs,
+                        plot_all)
 from configs import plots
 import argparse
 
@@ -29,11 +27,11 @@ def main() -> None:
                          save_dir=args.save_dir)
     
     elif args.plot == "ASW":
-
+        plot_asw(metrics_data_frame, configs=plots, save_dir=None, ax=None)
     elif args.plot == "cell_type_accuracy":
-
+        plot_cell_type_accuracy(metrics_data_frame, configs=plots, save_dir=None, ax=None)
     elif args.plot == "median_rank":
-
+        plot_median_rank(metrics_data_frame, configs=plots, save_dir=None, ax=None)
     elif args.plot == "tsne_original":
 
     elif args.plot == "tsne_embs":
