f41991885166dd29bdc280fa4bf72a185d34d9ba
Amir Ebrahimi <amir.ebrahimi01@ut.ac.ir>
2025-04-04 20:26:33 +0330

    Tuple typing added

--- sCIN/assess.py
+++ sCIN/assess.py
@@ -1,77 +1,15 @@
 import numpy as np
 from sklearn.metrics import silhouette_score
-from models.ConAAE.conAAE import mink
 from scipy.spatial.distance import cdist
+from typing import Tuple
 
-
-def compute_metrics(mod1_embs:np.array, mod2_embs:np.array, labels:np.array):
-
-    cell_num = mod1_embs.shape[0]
-    pairlist=[]
-    classlist=[]
-    distlist=[]
-    flag=np.zeros(cell_num)
-    kright10=0
-    kright20=0
-    kright30=0
-    kright40=0
-    kright50=0
-      
-    for index,i in enumerate(mod1_embs):
-      mini=999999
-      minindex=0
-      distlist.clear()
-      for idx,j in enumerate(mod2_embs):
-        dist=np.linalg.norm(i-j)
-        distlist.append(dist)
-          #print(dist)
-        if(dist<mini and flag[idx]==0):
-          mini=dist
-          minindex=idx
-      kindex10=mink(distlist,10)
-      kindex20=mink(distlist,20)
-      kindex30=mink(distlist,30)
-      kindex40=mink(distlist,40)
-      kindex50=mink(distlist,50)
-      if(index in kindex10):
-        kright10+=1
-      if(index in kindex20):
-        kright20+=1
-      if(index in kindex30):
-        kright30+=1
-      if(index in kindex40):
-        kright40+=1
-      if(index in kindex50):
-        kright50+=1
-      flag[minindex]=1
-      pairlist.append(minindex)
-      classlist.append(labels[minindex])
-      
-    ATAC_seq=np.arange(0,cell_num,1)
-      
-    pairlist=np.array(pairlist)
-            
-    classlist=np.array(classlist)
-
-    recall_at_k = {10: 0, 20: 0, 30: 0, 40: 0, 50: 0}
-    recall_at_k[10] = float(kright10)/cell_num
-    recall_at_k[20] = float(kright20)/cell_num
-    recall_at_k[30] = float(kright30)/cell_num
-    recall_at_k[40] = float(kright40)/cell_num
-    recall_at_k[50] = float(kright50)/cell_num
-    num_pairs = float(np.sum(pairlist==ATAC_seq))
-    cell_type_acc = float(np.sum(labels==classlist)/cell_num)
-
-    return recall_at_k, num_pairs, cell_type_acc
-
-########################################################################
 
 def compute_distance(mod1_embs:np.array, mod2_embs:np.array) -> np.array:
        
     return cdist(mod1_embs, mod2_embs, metric="euclidean")
    
 
-def sort_match_cells(dist_matrix:np.array) -> tuple[np.array, np.array, np.array]:
+def sort_match_cells(dist_matrix:np.array) -> Tuple[np.array, np.array, np.array]:
    
    num_cells = dist_matrix.shape[0]
    paired_cells = np.full(num_cells, -1)
@@ -173,7 +111,7 @@ def compute_MedR(mod1_embs:np.ndarray, mod2_embs:np.ndarray) -> float:
 
 
 def assess(mod1_embs:np.array, mod2_embs:np.array, 
-           labels:np.array, seed:int=None) -> tuple[dict, int, float, float]:
+           labels:np.array, seed:int=None) -> Tuple[dict, int, float, float]:
    
    dist_matrix = compute_distance(mod1_embs, mod2_embs)
    paired_cells, sorted_indices_all = sort_match_cells(dist_matrix)
